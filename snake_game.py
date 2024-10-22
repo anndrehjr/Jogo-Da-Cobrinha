02f6d9124b7f4d6a65baecc3e38d518e2029fe69
Andre_Junior <42242398+anndrehjr@users.noreply.github.com>
2024-10-22 00:06:14 -0300

    Initial commit

---
+++ snake_game.py
@@ -0,0 +1,134 @@
+import pygame
+import time
+import random
+
+pygame.init()
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+WIDTH = 600
+HEIGHT = 400
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+snake_block = 20
+snake_speed = 15
+
+def show_score(score):
+    value = pygame.font.SysFont(None, 35).render("Pontuação: " + str(score), True, WHITE)
+    SCREEN.blit(value, [0, 0])
+
+def draw_snake(snake_list):
+    for x in snake_list:
+        pygame.draw.rect(SCREEN, GREEN, [x[0], x[1], snake_block, snake_block])
+
+def message(msg, color):
+    mesg = pygame.font.SysFont(None, 30).render(msg, True, color)
+    SCREEN.blit(mesg, [WIDTH / 6, HEIGHT / 3])
+
+def game_loop():
+    game_over = False
+    game_close = False
+
+    x1 = WIDTH / 2
+    y1 = HEIGHT / 2
+    x1_change = 0
+    y1_change = 0
+
+    snake_list = []
+    length_of_snake = 1
+    score = 0
+
+    foodx = round(random.randrange(0, WIDTH - snake_block) / 20.0) * 20.0
+    foody = round(random.randrange(0, HEIGHT - snake_block) / 20.0) * 20.0
+
+    while not game_over:
+        while game_close == True:
+            SCREEN.fill(BLACK)
+            message("Você perdeu! Pressione C para continuar ou Q para sair.", RED)
+            show_score(score)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_c:
+                        game_loop()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    y1_change = -snake_block
+                    x1_change = 0
+                elif event.key == pygame.K_DOWN:
+                    y1_change = snake_block
+                    x1_change = 0
+
+        if x1 >= WIDTH or x1 < 0 or y1 >= HEIGHT or y1 < 0:
+            game_close = True
+
+        x1 += x1_change
+        y1 += y1_change
+
+        SCREEN.fill(BLACK)
+
+        pygame.draw.rect(SCREEN, RED, [foodx, foody, snake_block, snake_block])
+
+        snake_head = []
+        snake_head.append(x1)
+        snake_head.append(y1)
+        snake_list.append(snake_head)
+        if len(snake_list) > length_of_snake:
+            del snake_list[0]
+
+        for x in snake_list[:-1]:
+            if x == snake_head:
+                game_close = True
+
+        draw_snake(snake_list)
+
+        show_score(length_of_snake - 1)
+
+        pygame.display.update()
+
+        if x1 == foodx and y1 == foody:
+            foodx = round(random.randrange(0, WIDTH - snake_block) / 20.0) * 20.0
+            foody = round(random.randrange(0, HEIGHT - snake_block) / 20.0) * 20.0
+            length_of_snake += 1
+            score += 1
+
+        time.sleep(0.1)
+
+    pygame.quit()
+
+def start_screen():
+    while True:
+        SCREEN.fill(BLACK)
+        message("Bem-vindo ao Jogo da Cobrinha!", WHITE)
+        message("Pressione S para começar ou Q para sair", WHITE)
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    pygame.quit()
+                    quit()
+                if event.key == pygame.K_s:
+                    game_loop()
+
+start_screen()
